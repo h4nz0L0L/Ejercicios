3e7ef1282313b2308a423d7eb761502b05996bcc
Telmexhub <telmexhub@MacBook-Pro-de-Telmexhub.local>
2018-07-31 14:36:03 -0500

    pagina-Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeaturesComponent } from './features/features.component';
+import { DocsComponent } from './docs/docs.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { EventsComponent } from './events/events.component';
+import { BlogComponent } from './blog/blog.component';
+import { PortadaComponent } from './portada/portada.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ContenidoComponent } from './contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturesComponent,
+    DocsComponent,
+    ResourcesComponent,
+    EventsComponent,
+    BlogComponent,
+    PortadaComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule
